7b27dba311de9fed1eaa3714c0cecbe9afab0c56
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-22 20:48:30 -0600

    Add change_include

--- Figura.cpp
+++ Figura.cpp
@@ -13,7 +13,7 @@ Figura::Figura()
     numPuntos = 0;
     
 }
-Figura::Figura(std::string nom, std::list<Punto> pts, int num)
+Figura::Figura(std::string nom, std::vector<Punto> pts, int num)
 {
     nombre = nom;
     puntos = pts;
@@ -28,7 +28,7 @@ std::string Figura::getnombre()
 {
     return nombre;
 }
-std::list<Punto> Figura::getpuntos()
+std::vector<Punto> Figura::getpuntos()
 {
     return puntos;
 }
@@ -53,5 +53,12 @@ void Figura::printFigura()
     std::cout << "Nombre de la figura: " << nombre << std::endl;
     std::cout << "Numero de Puntos: " << numPuntos << std::endl;
     std::cout << "Puntos (ordenados por coordenada x, descendente)" << std::endl;
-    
+    std::vector<Punto> puntosOrdenados(puntos);
+    std::sort(puntosOrdenados.begin(), puntosOrdenados.end(), [](Punto& p1, Punto& p2) { return p1.getx() > p2.getx(); });
+
+    for (Punto& punto : puntosOrdenados)
+    {
+        std::cout << "x: " << punto.getx() << ", y: " << punto.gety() << std::endl;
+    }
+
 }
