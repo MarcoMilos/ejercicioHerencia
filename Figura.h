935b948265e3d7176ed385b557e3db453d137c99
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-23 20:47:18 -0600

    Add change_vectorTolist

--- Figura.h
+++ Figura.h
@@ -5,7 +5,7 @@
 #ifndef Figura_h
 #define Figura_h
 
-#include <vector>
+#include <list>
 #include <string>
 #include "Punto.h"
 
@@ -13,16 +13,16 @@ class Figura
 {
     // Declaramos los atributos privados
     std::string nombre;
-    std::vector<Punto> puntos;
+    std::list<Punto> puntos;
     int numPuntos;
     public:
         // Declaramos los constructores
         Figura();
-        Figura(std::string, std::vector<Punto>, int);
+        Figura(std::string, std::list<Punto>, int);
         ~Figura();
         // Metodo Getters
         std::string getnombre();
-        std::vector<Punto> getpuntos();
+        std::list<Punto> getpuntos();
         int getnumPuntos();
         // Metodo Setters
         void setNombre(std::string);
